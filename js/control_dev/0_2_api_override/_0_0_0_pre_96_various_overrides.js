44e61c36541e9ab2b983b0c75edd4cceba45c98a
nikeo <nicolas@presscustomizr.com>
2016-10-08 16:38:21 +0200

    removed the onChangeActive fix

--- js/control_dev/0_2_api_override/_0_0_0_pre_96_various_overrides.js
+++ js/control_dev/0_2_api_override/_0_0_0_pre_96_various_overrides.js
@@ -1,28 +1,5 @@
 
 (function (api, $, _) {
-
-  //FIX FOR CONTROL VISIBILITY LOST ON PREVIEW REFRESH #1
-  //This solves the problem of control visiblity settings being lost on preview refresh since WP 4.3
-  //this overrides the wp method only for the control instances
-  //it check if there's been a customizations
-  //=> args.unchanged is true for all cases, for example when api.previewer.loading and the preview send 'ready'created during the frame synchronisation
-  api.Control.prototype.onChangeActive = function ( active, args ) {
-        if ( args.unchanged )
-          return;
-        if ( this.container[0] && ! $.contains( document, this.container[0] ) ) {
-          // jQuery.fn.slideUp is not hiding an element if it is not in the DOM
-          this.container.toggle( active );
-          if ( args.completeCallback ) {
-            args.completeCallback();
-          }
-        } else if ( active ) {
-          this.container.slideDown( args.duration, args.completeCallback );
-        } else {
-          this.container.slideUp( args.duration, args.completeCallback );
-        }
-  };
-
-
    /* monkey patch for the content height set */
   //wp.customize.Section is not available before wp 4.1
   if ( 'function' == typeof api.Section ) {
