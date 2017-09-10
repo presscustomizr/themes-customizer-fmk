89a1d5eb73bc781a1c48d6afc127f63dabd24dfe
nikeo <nicolas@presscustomizr.com>
2017-09-10 15:40:35 +0200

    added : margin-bottom to relevant panels added : pro labels before relevant customizer panels and sections when czr_fn_is_pro()

--- js/control_dev/_0_pre_base.js
+++ js/control_dev/_0_pre_base.js
@@ -389,7 +389,19 @@ var czr_debug = {
       * ADD PRO BEFORE SPECIFIC SECTIONS AND PANELS
       *****************************************************************************/
       if ( serverControlParams.isPro ) {
-            _.each( [ 'tc_font_customizer_settings', 'header_image_sec', 'content_blog_sec', 'static_front_page', 'content_single_sec' ], function( _secId ) {
+            _.each( [
+                  'tc_font_customizer_settings',//WFC
+
+                  'header_image_sec',//hueman pro
+                  'content_blog_sec',//hueman pro
+                  'static_front_page',//hueman pro
+                  'content_single_sec',//hueman pro
+
+                  'tc_fpu',//customizr-pro
+                  'nav',//customizr-pro
+                  'post_lists_sec'//customizr-pro
+
+            ], function( _secId ) {
                   _.delay( function() {
                       api.section.when( _secId, function( _sec_ ) {
                             if ( 1 >= _sec_.headContainer.length ) {
@@ -398,7 +410,14 @@ var czr_debug = {
                       });
                   }, 1000 );
             });
-            _.each( ['hu-header-panel', 'hu-content-panel' ], function( _secId ) {
+            _.each( [
+                  'hu-header-panel',//hueman pro
+                  'hu-content-panel',//hueman pro
+
+                  'tc-header-panel',//customizr-pro
+                  'tc-content-panel',//customizr-pro
+                  'tc-footer-panel'//customizr-pro
+            ], function( _secId ) {
                   api.panel.when( _secId, function( _sec_ ) {
                         if ( 1 >= _sec_.headContainer.length ) {
                             _sec_.headContainer.find('.accordion-section-title').prepend( '<span class="pro-title-block">Pro</span>' );
@@ -409,7 +428,7 @@ var czr_debug = {
 
 
       /*****************************************************************************
-      * ADD PRO BEFORE SPECIFIC SECTIONS AND PANELS
+      * PRO SECTION CONSTRUCTOR
       *****************************************************************************/
       if ( ! serverControlParams.isPro && _.isFunction( api.Section ) ) {
             proSectionConstructor = api.Section.extend( {
