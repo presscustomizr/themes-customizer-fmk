b57449bb64cff4e55a69d1726192024a5a974e25
nikeo <nicolas@presscustomizr.com>
2018-03-17 18:55:58 +0100

    improved the dynamic registration args structure

--- js/base-fmk/_0_4_api_ready_dynamic_registration.js
+++ js/base-fmk/_0_4_api_ready_dynamic_registration.js
@@ -10,7 +10,7 @@
                 api.errorLog( 'serverControlParams.dynamicSettingParams should be an array');
           }
 
-          //console.log( 'serverControlParams.dynamicSettingParams', serverControlParams.dynamicSettingParams );
+          console.log( 'serverControlParams.dynamicSettingParams', serverControlParams.dynamicSettingParams );
 
           _.each( serverControlParams.dynamicSettingParams, function( dynParams, setId ) {
                 try { registerDynamicModuleSettingControl( dynParams ); } catch( er ) {
@@ -26,33 +26,73 @@
       var registerDynamicModuleSettingControl = function( args ) {
             args = _.extend( {
                   'setting_id' : '',
-                  'option_value'  : [],
-                  'section' : { id : '', label : '' },
                   'module_type' : '',
-                  'setting_type' : 'option'
+                  'option_value'  : [],
+                  // 'setting' => array(
+                  //     'type' => 'option',
+                  //     'default'  => array(),
+                  //     'transport' => 'refresh',
+                  //     'setting_class' => '',//array( 'path' => '', 'name' => '' )
+                  //     'sanitize_callback' => '',
+                  //     'validate_callback' => '',
+                  // ),
+                  'setting' : {},
+                  'section' : { id : '', title : '' },
+                  'control' : {},
+                  //'setting_type' : 'option'
+
             }, args );
 
             // we must have not empty setting_id, section and module_type
-            if ( _.isEmpty( args[ 'setting_id'] ) || _.isEmpty( args[ 'section'] ) || _.isEmpty( args[ 'module_type'] ) ) {
+            if ( _.isEmpty( args.setting_id ) || _.isEmpty( args.section) || _.isEmpty( args.module_type ) ) {
                   throw new Error( 'registerDynamicModuleSettingControl => missing params when registrating a setting');
             }
 
             // the option value must be an array
-            if ( ! _.isArray( args[ 'option_value']) ) {
+            if ( ! _.isArray( args.option_value ) ) {
                   throw new Error( 'registerDynamicModuleSettingControl => the module values must be an array');
             }
 
             // console.log( "args?", args );
-            var settingId =  args[ 'setting_id' ],
-                sectionData = args[ 'section' ];
+            var settingId =  args.setting_id,
+                settingArgs = args.setting;
 
-            // Check if we have a correct section
-            if ( ! _.has( sectionData, 'id' ) || ! _.has( sectionData, 'title' ) ) {
-                  throw new Error( 'registerDynamicModuleSettingControl => wrong params for the setting section');
+            // MAYBE REGISTER THE SETTING
+            // Register only if not registered already
+            // For example, when saved as draft in a changeset, the setting is already dynamically registered server side
+            // => in this case, we only need to register the associated control
+            if ( ! api.has( settingId ) ) {
+                  settingArgs = _.extend(
+                        {
+                              dirty : false,
+                              value : [],
+                              previewer: api.previewer,
+                              transport : 'refresh',
+                              type : 'option',
+                        },
+                        {
+                              value : args.option_value,
+                              transport : settingArgs.transport,
+                              type : settingArgs.type
+                        }
+                  );
+                  // assign the value sent from the server
+                  settingArgs.value = args.option_value;
+
+                  // console.log('registerDynamicModuleSettingControl => SETTING DATA ?', settingId, settingArgs);
+                  var SettingConstructor = api.settingConstructor[ settingArgs.type ] || api.Setting;
+                  api.add( new SettingConstructor( settingId, settingArgs.value, settingArgs ) );
             }
 
             // MAYBE REGISTER THE SECTION
-            if ( ! api.section.has( sectionData.id ) ) {
+            var sectionArgs = args.section;
+
+            // Check if we have a correct section
+            if ( ! _.has( sectionArgs, 'id' ) || ! _.has( sectionArgs, 'title' ) ) {
+                  throw new Error( 'registerDynamicModuleSettingControl => wrong params for the setting section');
+            }
+
+            if ( ! api.section.has( sectionArgs.id ) ) {
                   var _secData = _.extend(
                       {
                         active:true,
@@ -67,9 +107,11 @@
                         title: "",
                         type: "default",
                       }, {
-                        id: sectionData.id,
-                        title: sectionData.title,
-                        panel: _.isEmpty( sectionData.title ) ? '' : sectionData.panel
+                        id: sectionArgs.id,
+                        title: sectionArgs.title,
+                        description: _.isEmpty( sectionArgs.description ) ? '' : sectionArgs.description,
+                        panel: _.isEmpty( sectionArgs.panel ) ? '' : sectionArgs.panel,
+                        priority: sectionArgs.priority
                       }
                   );
 
@@ -78,58 +120,39 @@
                   api.section.add( new Constructor( _secData.id, _secData ) );
             }
 
-            // MAYBE REGISTER THE SETTING
-            // Register only if not registered already
-            // For example, when saved as draft in a changeset, the setting is already dynamically registered server side
-            // => in this case, we only need to register the associated control
-            if ( ! api.has( settingId ) ) {
-                  var settingData = _.extend(
-                        {
-                              transport : 'refresh',
-                              type : 'option',
-                        }, {
-                              dirty : false,
-                              value : args[ 'option_value' ],
-                              previewer: api.previewer
-                        }
-                  );
-
-                  // console.log('registerDynamicModuleSettingControl => SETTING DATA ?', settingId, settingData);
-                  var SettingConstructor = api.settingConstructor[ settingData.type ] || api.Setting;
-                  api.add( new SettingConstructor( settingId, settingData.value, settingData ) );
-            }
-
             // REGISTER THE CONTROL
             var controlId = settingId;
             // start from a copy of a core control object
-            var controlData = $.extend( true, {} , api.settings.controls.blogdescription );
+            var controlArgs = args.control,
+                defaultControlArgs = $.extend( true, {} , api.settings.controls.blogdescription );
             // Then update it with our defaults set server side
             // array(
             //     'type'      => 'czr_module',
             //     'module_type' => 'czr_flat_skope_module',
             //     'section'   => 'flat_skope_sec'
             // );
-            controlData = _.extend(
-                  controlData,
+            controlArgs = _.extend(
+                  defaultControlArgs,
                   {
                         type : 'czr_module',
-                        module_type : args[ 'module_type'],
-                        section : sectionData.id,
+                        module_type : args.module_type,
+                        section : sectionArgs.id,
                         content : '',
-                        label : args[ 'control_label']
+                        label : controlArgs.label,
+                        priority : controlArgs.priority
                   }
             );
 
             // Then associates the settingId
-            controlData.settings.default = settingId;
+            controlArgs.settings.default = settingId;
 
-            var ControlConstructor = api.controlConstructor[ controlData.type ] || api.Control, options;
-            options = _.extend( { params: controlData }, controlData ); // Inclusion of params alias is for back-compat for custom controls that expect to augment this property.
+            var ControlConstructor = api.controlConstructor[ controlArgs.type ] || api.Control, options;
+            options = _.extend( { params: controlArgs }, controlArgs ); // Inclusion of params alias is for back-compat for custom controls that expect to augment this property.
             var _ctrl_ = api.control.add( new ControlConstructor( controlId, options ) );
 
             // if the currently expanded section is the one of the dynamic control
             // Awake the module => fire ready
-            if ( api.section( sectionData.id ).expanded() ) {
+            if ( api.section( sectionArgs.id ).expanded() ) {
                   api.control( controlId ).trigger( 'set-module-ready' );
             }
             // console.log('registerDynamicModuleSettingControl => CONTROL DATA ?', settingId, options);
