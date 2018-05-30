893ccccd176075f2abf2878dd6fb01ea0b4b6879
nikeo <nicolas@presscustomizr.com>
2018-05-30 23:44:47 +0200

    fixed : set some properties of the module candidate to default before instantiation. For example "hasPreItem".

--- js/base-fmk/1_0_input_item_module_control/1_4_control/base_module_control/_0_1_1_base_module_control_instantiate.js
+++ js/base-fmk/1_0_input_item_module_control/1_4_control/base_module_control/_0_1_1_base_module_control_instantiate.js
@@ -125,7 +125,7 @@ $.extend( CZRBaseModuleControlMths, {
                         case 'crud' :
                               //get the value from the czrModuleMap
                               if ( _.has( api.czrModuleMap, module_candidate.module_type ) ) {
-                                    _candidate_val = api.czrModuleMap[ module_candidate.module_type ].crud;
+                                    _candidate_val = api.czrModuleMap[ module_candidate.module_type ].crud || _value;
                               } else if ( ! _.isUndefined( _candidate_val) && ! _.isBoolean( _candidate_val )  ) {
                                     throw new Error('prepareModuleForAPI : the module param "crud" must be a boolean');
                               }
@@ -134,7 +134,7 @@ $.extend( CZRBaseModuleControlMths, {
                         case 'hasPreItem' :
                               //get the value from the czrModuleMap
                               if ( _.has( api.czrModuleMap, module_candidate.module_type ) ) {
-                                    _candidate_val = api.czrModuleMap[ module_candidate.module_type ].hasPreItem;
+                                    _candidate_val = api.czrModuleMap[ module_candidate.module_type ].hasPreItem || _value;
                               } else if ( ! _.isUndefined( _candidate_val) && ! _.isBoolean( _candidate_val )  ) {
                                     throw new Error('prepareModuleForAPI : the module param "hasPreItem" must be a boolean');
                               }
@@ -143,7 +143,7 @@ $.extend( CZRBaseModuleControlMths, {
                         case 'refresh_on_add_item' :
                               //get the value from the czrModuleMap
                               if ( _.has( api.czrModuleMap, module_candidate.module_type ) ) {
-                                    _candidate_val = api.czrModuleMap[ module_candidate.module_type ].refresh_on_add_item;
+                                    _candidate_val = api.czrModuleMap[ module_candidate.module_type ].refresh_on_add_item || _value;
                               } else if ( ! _.isUndefined( _candidate_val) && ! _.isBoolean( _candidate_val )  ) {
                                     throw new Error('prepareModuleForAPI : the module param "refresh_on_add_item" must be a boolean');
                               }
