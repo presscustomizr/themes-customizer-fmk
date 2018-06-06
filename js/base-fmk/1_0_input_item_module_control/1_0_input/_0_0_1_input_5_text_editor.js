96b8667e39844490c968828a290e54eb99850043
nikeo <nicolas@presscustomizr.com>
2018-06-06 16:09:07 +0200

    fix : tinymce not reacting to changes made to the text editor ( html code )

--- js/base-fmk/1_0_input_item_module_control/1_0_input/_0_0_1_input_5_text_editor.js
+++ js/base-fmk/1_0_input_item_module_control/1_0_input/_0_0_1_input_5_text_editor.js
@@ -10,13 +10,26 @@ $.extend( CZRInputMths , {
             }
 
             // This event is triggered from sektions::setupTinyMceEditor()
-            input.input_parent.control.bind( 'tinyMceEditorUpdated', function() {
+            // @params = {
+            //    input_id : '',
+            //    html_content : '',
+            //    modified_editor_element : ''//<= can be the visual / text editor tab
+            // }
+            // Note : this event is trigger when the visual AND the text tab of the editor are modified.
+            // This means that the modified_editor_element can be different if it is the visual editor ( or the text html editor <textarea> element
+            // @see sektions::setupTinyMceEditor()
+            input.input_parent.control.bind( 'tinyMceEditorUpdated', function( params ) {
                   //console.log('in input => tinyMceEditorUpdated ', api.sekEditorSynchronizedInput(), input.input_parent.control.id );
-                  //console.log('/// api.sekEditorSynchronizedInput', api.sekEditorSynchronizedInput );
+                  //console.log('/// api.sekEditorSynchronizedInput', params );
                   if ( api.sekEditorSynchronizedInput().control_id != input.input_parent.control.id || api.sekEditorSynchronizedInput().input_id != input.id )
                     return;
-                  input( wp.editor.removep( api.sekTinyMceEditor.getContent() ) );
-                  api.sekTinyMceEditor.focus();
+                  input( wp.editor.removep( params.html_content || api.sekTinyMceEditor.getContent() ) );
+                  if ( params.modified_editor_element && params.modified_editor_element.length > 0 ) {
+                        params.modified_editor_element.focus();
+                  } else {
+                        api.sekTinyMceEditor.focus();
+                  }
+
             });
 
       },//setupTextEditor
