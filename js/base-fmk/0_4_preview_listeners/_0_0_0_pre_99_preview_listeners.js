57c3b6ca43af28d637c5a03e4f55fa7f87cc5bbd
nikeo <nicolas@presscustomizr.com>
2018-06-13 21:02:39 +0200

    moved widget_area module specific code to the Hueman theme

--- js/base-fmk/0_4_preview_listeners/_0_0_0_pre_99_preview_listeners.js
+++ js/base-fmk/0_4_preview_listeners/_0_0_0_pre_99_preview_listeners.js
@@ -5,37 +5,6 @@
   *****************************************************************************/
   //Data are sent by the preview frame when the panel has sent the 'sync' or even better 'active' event
   api.bind( 'ready', function() {
-        //observe widget settings changes
-        api.previewer.bind('houston-widget-settings', function(data) {
-              //get the difference
-              var _candidates = _.filter( data.registeredSidebars, function( sb ) {
-                return ! _.findWhere( _wpCustomizeWidgetsSettings.registeredSidebars, { id: sb.id } );
-              });
-
-              var _inactives = _.filter( data.registeredSidebars, function( sb ) {
-                return ! _.has( data.renderedSidebars, sb.id );
-              });
-
-              _inactives = _.map( _inactives, function(obj) {
-                return obj.id;
-              });
-
-              var _registered = _.map( data.registeredSidebars, function(obj) {
-                return obj.id;
-              });
-
-              //stores and update the widget zone settings
-              api.czr_widgetZoneSettings = api.czr_widgetZoneSettings || new api.Value();//will store all widget zones data sent by preview as an observable object
-              api.czr_widgetZoneSettings.set( {
-                    actives :  data.renderedSidebars,
-                    inactives :  _inactives,
-                    registered :  _registered,
-                    candidates :  _candidates,
-                    available_locations :  data.availableWidgetLocations//built server side
-              } );
-
-        });
-
         /* WP CONDITIONAL TAGS => stores and observes the WP conditional tags sent by the preview */
         api.previewer.bind( 'czr-query-data-ready', function( data ) {
               api.czr_wpQueryInfos = api.czr_wpQueryInfos || new api.Value();
