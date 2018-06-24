97e6801ee9fc5e0dfad5ba1bbe181c91a6aaf4ee
nikeo <nicolas@presscustomizr.com>
2018-06-24 15:41:15 +0200

    normalized the registration of php registered setting / control / section with api.CZR_Helpers.register()

--- js/base-fmk/_0_4_api_ready_dynamic_registration.js
+++ js/base-fmk/_0_4_api_ready_dynamic_registration.js
@@ -13,8 +13,12 @@
           _.each( serverControlParams.paramsForDynamicRegistration, function( dynParams, setId ) {
                 // The dynamic registration should be explicitely set
                 if ( dynParams.module_registration_params && true === dynParams.module_registration_params.dynamic_registration ) {
-                      try { registerDynamicModuleSettingControl( dynParams ); } catch( er ) {
-                            api.errorLog( er );
+                      if ( serverControlParams.isDevMode ) {
+                            registerDynamicModuleSettingControl( dynParams );
+                      } else {
+                            try { registerDynamicModuleSettingControl( dynParams ); } catch( er ) {
+                                  api.errorLog( er );
+                            }
                       }
                 }
           });
@@ -58,31 +62,15 @@
             var settingId =  args.setting_id,
                 settingArgs = args.setting;
 
-            // MAYBE REGISTER THE SETTING
-            // Register only if not registered already
-            // For example, when saved as draft in a changeset, the setting is already dynamically registered server side
-            // => in this case, we only need to register the associated control
-            if ( ! api.has( settingId ) ) {
-                  settingArgs = _.extend(
-                        {
-                              dirty : false,
-                              value : [],
-                              previewer: api.previewer,
-                              transport : 'refresh',
-                              type : 'option',
-                        },
-                        {
-                              value : args.option_value,
-                              transport : settingArgs.transport || 'refresh',
-                              type : settingArgs.type || 'option'
-                        }
-                  );
-                  // assign the value sent from the server
-                  settingArgs.value = args.option_value;
-
-                  var SettingConstructor = api.settingConstructor[ settingArgs.type ] || api.Setting;
-                  api.add( new SettingConstructor( settingId, settingArgs.value, settingArgs ) );
-            }
+            api.CZR_Helpers.register( {
+                  what : 'setting',
+                  id : settingId,
+                  dirty : ! _.isEmpty( args.option_value ),
+                  value : args.option_value,
+                  transport : settingArgs.transport || 'refresh',
+                  type : settingArgs.type || 'option',
+                  track : false// <= don't add it in any registered collection @see Nimble or Contextualizer
+            });
 
             // MAYBE REGISTER THE SECTION
             var sectionArgs = args.section;
@@ -92,88 +80,49 @@
                         throw new Error( 'registerDynamicModuleSettingControl => missing section id for the section of setting : ' + settingId );
                   }
 
-                  if ( ! api.section.has( sectionArgs.id ) ) {
-                        var _secData = _.extend(
-                            {
-                              active:true,
-                              content:"",
-                              customizeAction:"Customizing",
-                              description:"",
-                              description_hidden:false,
-                              id: "",
-                              instanceNumber: 99,
-                              panel: "",
-                              priority:0,
-                              title: "",
-                              type: "default",
-                            }, {
-                              id: sectionArgs.id,
-                              title: sectionArgs.title || sectionArgs.id,
-                              description: _.isEmpty( sectionArgs.description ) ? '' : sectionArgs.description,
-                              panel: _.isEmpty( sectionArgs.panel ) ? '' : sectionArgs.panel,
-                              priority: sectionArgs.priority || 10
-                            }
-                        );
-
-                        var Constructor = api.sectionConstructor[ _secData.type ] || api.Section;
-                        _secData = _.extend( { params: _secData }, _secData ); // Inclusion of params alias is for back-compat for custom sections that expect to augment this property.
-                        api.section.add( new Constructor( _secData.id, _secData ) );
-                  }
+                  api.CZR_Helpers.register({
+                        what : 'section',
+                        id : sectionArgs.id,
+                        title: sectionArgs.title || sectionArgs.id,
+                        panel : _.isEmpty( sectionArgs.panel ) ? '' : sectionArgs.panel,
+                        priority : sectionArgs.priority || 10,
+                        track : false// <= don't add it in any registered collection @see Nimble or Contextualizer => this will prevent this container to be removed when cleaning the registered
+                  });
             }
 
             // REGISTER THE CONTROL
-            var controlId = settingId;
+            var controlId = settingId,
+                controlArgs = args.control,
+                ctrlSectionId;
 
-            if ( ! api.control.has( controlId ) ) {
+            // Do we have a section ?
+            if ( ! _.isEmpty( args.section ) ) {
+                  ctrlSectionId = args.section.id;
+            } else {
+                  ctrlSectionId = controlArgs.section;
+            }
 
+            if ( _.isEmpty( ctrlSectionId ) ) {
+                  api.errare( 'registerDynamicModuleSettingControl => missing section id for the control', args );
+                  throw new Error( 'registerDynamicModuleSettingControl => missing section id for the section of setting : ' + settingId );
+            }
+            api.CZR_Helpers.register({
+                  what : 'control',
+                  id : controlId,
+                  label : controlArgs.label || controlId,
+                  type : 'czr_module',
+                  module_type : args.module_type,//'czr_background',
+                  section : ctrlSectionId,//'contx_body_bg',
+                  priority : controlArgs.priority || 10,
+                  settings : { default : settingId },
+                  track : false// <= don't add it in any registered collection @see Nimble or Contextualizer => this will prevent this container to be removed when cleaning the registered
+            });
 
-                  // start from a copy of a core control object
-                  var controlArgs = args.control,
-                      defaultControlArgs = $.extend( true, {} , api.settings.controls.blogdescription ),
-                      ctrlSectionId;
-
-                  // Do we have a section ?
-                  if ( ! _.isEmpty( args.section ) ) {
-                        ctrlSectionId = args.section.id;
-                  } else {
-                        ctrlSectionId = controlArgs.section;
-                  }
-
-                  if ( _.isEmpty( ctrlSectionId ) ) {
-                        api.errare( 'registerDynamicModuleSettingControl => missing section id for the control', args );
-                        throw new Error( 'registerDynamicModuleSettingControl => missing section id for the section of setting : ' + settingId );
-                  }
-                  // Then update it with our defaults set server side
-                  // array(
-                  //     'type'      => 'czr_module',
-                  //     'module_type' => 'czr_flat_skope_module',
-                  //     'section'   => 'flat_skope_sec'
-                  // );
-                  controlArgs = _.extend(
-                        defaultControlArgs,
-                        {
-                              type : 'czr_module',
-                              module_type : args.module_type,
-                              section : ctrlSectionId,
-                              content : '',
-                              label : controlArgs.label,
-                              priority : controlArgs.priority
-                        }
-                  );
-
-                  // Then associates the settingId
-                  controlArgs.settings.default = settingId;
-
-                  var ControlConstructor = api.controlConstructor[ controlArgs.type ] || api.Control, options;
-                  options = _.extend( { params: controlArgs }, controlArgs ); // Inclusion of params alias is for back-compat for custom controls that expect to augment this property.
-                  var _ctrl_ = api.control.add( new ControlConstructor( controlId, options ) );
-
-                  // if the currently expanded section is the one of the dynamic control
-                  // Awake the module => fire ready
-                  if ( api.section( ctrlSectionId ).expanded() ) {
-                        api.control( controlId ).trigger( 'set-module-ready' );
-                  }
-            }//if ( ! api.control.has( controlId ) )
+            // if the currently expanded section is the one of the dynamic control
+            // Awake the module => fire ready
+            if ( api.section.has( ctrlSectionId ) && api.section( ctrlSectionId ).expanded() ) {
+                  api.control( controlId ).trigger( 'set-module-ready' );
+            }
 
             return settingId;
       };//registerDynamicModuleSettingControl
